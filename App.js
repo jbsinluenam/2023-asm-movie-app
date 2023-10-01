b92727b975f187d8fa9c8131d2a2340b5ab4be63
JB Sinluenam <jb.sinluenam@gmail.com>
2023-09-30 17:50:35 -0700

    Finish set up

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text className='text-red-600'>Hello World!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
---
+++ src/pages/screen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function screen() {
+  return (
+    <View>
+      <Text className='text-cyan-500'>screen</Text>
+    </View>
+  );
+}
